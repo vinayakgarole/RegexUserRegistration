a0bf75388a86b8be31384bba85a3b7b48e753242
vinayakgarole <vinayakgarole10@gamil.com>
2022-12-01 16:20:29 +0530

    Added: Welcome to User Registration Program

---
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class UserRegistrationMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to User Registration Program");
+    }
+}
