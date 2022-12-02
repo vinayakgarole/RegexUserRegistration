570a3d062ad05c8d3b98096a1e5ad827adcbdb1a
vinayakgarole <vinayakgarole10@gamil.com>
2022-12-02 22:36:54 +0530

    Added: JUnit Testing Cases

--- src/main/java/com/bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -6,40 +6,34 @@ public class UserRegistrationMain {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to User Registration Program");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the options:\n1. Validate First Name\n2. Validate Last Name\n3. Validate Email ID\n4. Validate Mobile Number" +
+                "\n5. Validate Password Rule1\n6. Validate Password Rule2\n7. Validate Password Rule3\n8. Validate Password Rule4");
 
-        UserRegistration user = new UserRegistration();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 1: To validate First Name:\nEnter 2: To validate Last Name:\nEnter 3: validate email:\nEnter 4: validate mobile number:" +
-                "\nEnter 5: validate password:\nEnter 6: validate oneUpperCase password:\nEnter 7: validate numericPassword:\nEnter 8: validate special password:\nEnter 9: validate EMail sample: ");
-
-        switch (sc.nextInt()) {
+        switch (scanner.nextInt()){
             case 1:
-                user.validFirstName();
+                UserRegistration.validFirstName("Vinayak");
                 break;
             case 2:
-                user.validLastName();
+                UserRegistration.validLastName("Garole");
                 break;
             case 3:
-                user.validEmail();
+                UserRegistration.validEmail("abc.xyz@bl.co.in");
                 break;
             case 4:
-                user.validMobileNumber();
+                UserRegistration.validMobileNumber("91 8380867601");
                 break;
             case 5:
-                user.validPassword();
+                UserRegistration.validPasswordRule1("Vianyak@10");
                 break;
             case 6:
-                user.validUpperCasePassword();
+                UserRegistration.validPasswordRule2("Vianyak@10-12");
                 break;
             case 7:
-                user.validNumericPassword();
+                UserRegistration.validPasswordRule3("Vianyak1@102");
                 break;
             case 8:
-                user.validSpecialPassword();
-                break;
-            case 9:
-                user.validEMailSample();
+                UserRegistration.validPasswordRule4("Vianyak10@10Garole");
                 break;
         }
     }
